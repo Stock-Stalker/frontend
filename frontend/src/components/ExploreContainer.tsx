db2eeb0d632f1f4ed16c916d3875628228e7a54a
Starlight Romero <starlightromero@protonmail.com>
2021-03-06 20:41:47 -0800

    [update] added React import

--- frontend/src/components/ExploreContainer.tsx
+++ frontend/src/components/ExploreContainer.tsx
@@ -1,13 +1,10 @@
+import React from 'react';
 import './ExploreContainer.css';
 
-interface ContainerProps {
-  name: string;
-}
-
-const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+const ExploreContainer: React.FC= () => {
   return (
     <div className="container">
-      <strong>{name}</strong>
+      <strong>Tab</strong>
       <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
     </div>
   );
