d2caf06030e5444dcc04c6ee1b247612f275a5de
Starlight Romero <starlightromero@protonmail.com>
2021-03-06 20:41:32 -0800

    [update] added React import

--- frontend/src/pages/Tab2.tsx
+++ frontend/src/pages/Tab2.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
@@ -16,7 +17,7 @@ const Tab2: React.FC = () => {
             <IonTitle size="large">Tab 2</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <ExploreContainer />
       </IonContent>
     </IonPage>
   );
--- frontend/src/pages/Tab3.tsx
+++ frontend/src/pages/Tab3.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
@@ -16,7 +17,7 @@ const Tab3: React.FC = () => {
             <IonTitle size="large">Tab 3</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        <ExploreContainer />
       </IonContent>
     </IonPage>
   );
