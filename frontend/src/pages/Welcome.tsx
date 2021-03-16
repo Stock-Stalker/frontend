28caec0ba60df9d4adb42851c776e557b3137500
tsamantanis <philip.tsamantanis@icloud.com>
2021-03-16 10:43:35 -0700

    Remove frontend starter code

--- frontend/src/pages/Welcome.tsx
+++ frontend/src/pages/Welcome.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-import './Tab3.css';
+import './Welcome.css';
 
-const Tab3: React.FC = () => {
+const Welcome: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Logo</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large">Logo</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
@@ -23,4 +23,4 @@ const Tab3: React.FC = () => {
   );
 };
 
-export default Tab3;
+export default Welcome;
