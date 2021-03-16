8a29aac301b12aac6629d1f766abe8db72d260de
tsamantanis <philip.tsamantanis@icloud.com>
2021-03-15 18:23:37 -0700

    Create and test custom theme colors and effects

--- frontend/src/components/ExploreContainer.tsx
+++ frontend/src/components/ExploreContainer.tsx
@@ -4,8 +4,10 @@ import './ExploreContainer.css';
 const ExploreContainer: React.FC= () => {
   return (
     <div className="container">
-      <strong>Tab</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <div className="glass-primary ellipse" style={{minHeight: '500px', width: '500px', margin: 'auto', paddingTop: '15%'}}>
+          <h1 className="neon-primary">LOREM IPSUM</h1>
+          <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      </div>
     </div>
   );
 };
