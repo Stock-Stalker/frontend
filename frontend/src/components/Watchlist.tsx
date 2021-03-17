d31a1b3391041bc17ea996ab02ba6fa5337c52a9
Starlight Romero <starlightromero@protonmail.com>
2021-03-17 16:57:23 -0700

    [create] Watchlist component

---
+++ frontend/src/components/Watchlist.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Watchlist.css'
+
+const Watchlist: React.FC = () => {
+    return (
+        <div className="watchlist-container">
+            <div className="watchlist-header">
+                <h4 className="watchlist-heading">Watchlist</h4>
+            </div>
+            <div className="watchlist">
+                <div className="watchlist-item">
+                    ABNB
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Watchlist
