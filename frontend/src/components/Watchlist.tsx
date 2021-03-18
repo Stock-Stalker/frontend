14a94302631719366421afa06c697114ecceb7ed
Starlight Romero <starlightromero@protonmail.com>
2021-03-17 17:28:19 -0700

    [update] Watchlist to have WatchlistItem

--- frontend/src/components/Watchlist.tsx
+++ frontend/src/components/Watchlist.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import WatchlistItem from './WatchlistItem'
 import './Watchlist.css'
 
 const Watchlist: React.FC = () => {
@@ -8,9 +9,10 @@ const Watchlist: React.FC = () => {
                 <h4 className="watchlist-heading">Watchlist</h4>
             </div>
             <div className="watchlist">
-                <div className="watchlist-item">
-                    ABNB
-                </div>
+                <WatchlistItem symbol="ABNB" price={189.23} prediction={true} />
+                <WatchlistItem symbol="GM" price={57.93} prediction={true} />
+                <WatchlistItem symbol="TSLA" price={690.41} prediction={false} />
+                <WatchlistItem symbol="VWAGY" price={42.34} prediction={false} />
             </div>
         </div>
     )
