2ed7902385a9a45efb04b55de26b39bdcb220407
Starlight Romero <starlightromero@protonmail.com>
2021-03-17 17:31:46 -0700

    [update] Stock bug fix

--- frontend/src/pages/Stock.tsx
+++ frontend/src/pages/Stock.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Layout from '../components/Layout'
-import Watchlist from '../components/Watchlist'
 import StockInfoChart from '../components/StockInfoChart'
 import StockPredictionEllipse from '../components/StockPredictionEllipse'
 import Watchlist from '../components/Watchlist'
