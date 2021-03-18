a4d379b4a08d37c9f88dfb87006f1c4cd25de2ce
Starlight Romero <starlightromero@protonmail.com>
2021-03-17 18:06:04 -0700

    [create] TimeFrameButton component

---
+++ frontend/src/components/TimeFrameButton.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './TimeFrameButton.css'
+
+interface TimeFrame {
+    timeFrame: string,
+    prediction: boolean
+}
+
+const TimeFrameButton: React.FC<TimeFrame> = ({ timeFrame, prediction }) => {
+    const color = prediction ? 'tertiary' : 'secondary'
+
+    return (
+        <div className={`timeFrameButton glass-${color}`}>
+            <p className={`text-${color}`}>{timeFrame}</p>
+        </div>
+    )
+}
+
+export default TimeFrameButton
