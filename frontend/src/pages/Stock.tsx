5b8701ebd575dc46e5d2ca593bc526a10ddfa6de
tsamantanis <philip.tsamantanis@icloud.com>
2021-03-17 17:07:37 -0700

    Minor watchlist styling fixes and merge conflict resolution

--- frontend/src/pages/Stock.tsx
+++ frontend/src/pages/Stock.tsx
@@ -1,12 +1,8 @@
 import React from 'react'
 import Layout from '../components/Layout'
-<<<<<<< HEAD
-import LineGraph from '../components/LineGraph'
 import Watchlist from '../components/Watchlist'
-=======
 import StockInfoChart from '../components/StockInfoChart'
 import StockPredictionEllipse from '../components/StockPredictionEllipse'
->>>>>>> 07928b37fb88a571df576a54d10512c9ea676c14
 import './Stock.css'
 
 const Stock: React.FC = () => {
@@ -19,13 +15,9 @@ const Stock: React.FC = () => {
                 </div>
                 <div className="col-3">
                     <StockPredictionEllipse prediction={ prediction } />
+                    <Watchlist />
                 </div>
             </div>
-<<<<<<< HEAD
-            <Watchlist />
-
-=======
->>>>>>> 07928b37fb88a571df576a54d10512c9ea676c14
         </Layout>
     )
 }
