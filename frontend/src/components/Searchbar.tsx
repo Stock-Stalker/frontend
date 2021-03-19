cff0cf29afeaba190d20b20ca0183f2527837246
Starlight Romero <starlightromero@protonmail.com>
2021-03-18 20:10:56 -0700

    [update] removed console.log

--- frontend/src/components/Searchbar.tsx
+++ frontend/src/components/Searchbar.tsx
@@ -19,17 +19,13 @@ const SearchBar: React.FC = () => {
     useEffect(() => {
         loadStockList()
     }, [])
-    console.log(stockList)
 
     const filteredStocks =
         stockList &&
-        stockList.filter(stock => {
+        stockList.filter((stock) => {
             const searchable = stock.companyName.split('-')[0] + stock.symbol
-            const searchTerms = searchTerm
-                .toLowerCase()
-                .trim()
-                .split(' ')
-            return searchTerms.every(term => {
+            const searchTerms = searchTerm.toLowerCase().trim().split(' ')
+            return searchTerms.every((term) => {
                 return searchable.toLowerCase().includes(term)
             })
         })
@@ -39,16 +35,16 @@ const SearchBar: React.FC = () => {
                 type="text"
                 placeholder="Search for a stock"
                 value={searchTerm}
-                onIonChange={e => setSearchTerm(e.detail.value!)}
-            ></IonSearchbar>
+                onIonChange={(e) =>
+                    setSearchTerm(e.detail.value!)
+                }></IonSearchbar>
             {searchTerm.length > 0 && (
                 <div className="search-results glass-input-primary">
                     {filteredStocks.length > 0 ? (
                         filteredStocks.map((stock, index) => (
                             <IonRouterLink
                                 href={'/stock/' + stock.symbol}
-                                key={stock.symbol + index.toString()}
-                            >
+                                key={stock.symbol + index.toString()}>
                                 <div className="result">
                                     <span>
                                         {stock.symbol +
