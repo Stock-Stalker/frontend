969a8c7cd0890940a3a3a7105033bef84ba3149a
Starlight Romero <starlightromero@protonmail.com>
2021-03-18 20:11:51 -0700

    [update] StockInfoChart to get data from api

--- frontend/src/components/StockInfoChart.tsx
+++ frontend/src/components/StockInfoChart.tsx
@@ -1,4 +1,6 @@
 import React, { useState, useCallback } from 'react'
+import { useParams } from 'react-router-dom'
+import { useQuery } from 'react-query'
 import api from '../api'
 import LineGraph from '../components/LineGraph'
 import TimeFrameButtons from '../components/TimeFrameButtons'
@@ -9,43 +11,124 @@ interface Prediction {
     prediction: boolean
 }
 
+interface ParamTypes {
+    symbol: string
+}
+
 const StockInfoChart: React.FC<Prediction> = ({ prediction }) => {
+    const [timeFrame, setTimeFrame] = useState<string>('D')
+    const { symbol } = useParams<ParamTypes>()
+
     const fetchStockData = useCallback(async () => {
-        const symbol = 'aapl'
         const res = await api.get(`/stocks/${symbol}`)
-        console.log(res.data)
+        return res.data
     }, [])
 
-    fetchStockData()
+    const { data, isLoading, isError } = useQuery('weekly', fetchStockData, {
+        retry: false,
+        staleTime: Infinity,
+    })
 
-    const color = prediction ? 'tertiary' : 'secondary'
-    const [timeFrame, setTimeFrame] = useState<string>('D')
-    return (
-        <>
-            <h3 className="company-name">
-                Apple <span className="company-symbol">APPL</span>
-            </h3>
-            <div className="glass-primary card card-stock">
-                <h3>$ 120.34</h3>
-                <span className={`text-${color}`}>
-                    <img
-                        width="30px"
-                        src={prediction ? arrowUp : arrowDown}
-                        alt="StockStalker Arrow"
-                    />{' '}
-                    $ 7.84 (5.53%)
-                </span>
-                <div className={`chart glass-${color}`}>
-                    <LineGraph prediction={prediction} />
+    if (isLoading) {
+        return <h3>Loading...</h3>
+    } else if (isError) {
+        return <h3>Something Went Wrong</h3>
+    } else {
+        const {
+            companyName,
+            currentPrice,
+            historicalData,
+            symbol,
+        } = data.stockData
+
+        const getGraphData = () => {
+            let days
+            let baseline: Array<String> = []
+            const graphData: Array<String> = []
+            switch (timeFrame) {
+                case 'D':
+                    days = 0
+                    for (let i = days; i >= 0; i--) {
+                        graphData.push(historicalData[i].open)
+                        graphData.push(historicalData[i].close)
+                    }
+                    baseline = new Array(1)
+                    baseline.fill(historicalData[days].open, 0, 1)
+                    return { graphData, baseline }
+                case 'W':
+                    days = 4
+                    break
+                case 'M':
+                    days = 20
+                    break
+                case '3M':
+                    days = 62
+                    break
+                case '6M':
+                    days = 125
+                    break
+                case 'Y':
+                    days = 251
+                    break
+                default:
+                    return { graphData, baseline }
+            }
+            for (let i = days; i >= 0; i--) {
+                graphData.push(historicalData[i].open)
+                graphData.push(historicalData[i].close)
+            }
+            const arraySize = days * 2 + 2
+            baseline = new Array(arraySize)
+            baseline.fill(historicalData[days].open, 0, arraySize)
+            return { graphData, baseline }
+        }
+
+        const { graphData, baseline } = getGraphData()
+
+        const open = graphData[0]
+        const close = graphData[graphData.length - 1]
+
+        const isPositive: boolean = +close - +open > 0
+        const changeSinceOpen: Number = Math.abs(+(+close - +open).toFixed(2))
+        const changePercentage: Number = +(
+            (Math.abs(+close - +open) / +open) *
+            100
+        ).toFixed(2)
+        const color = isPositive ? 'tertiary' : 'secondary'
+
+        return (
+            <>
+                <h3 className="company-name">
+                    {companyName}{' '}
+                    <span className="company-symbol">{symbol}</span>
+                </h3>
+                <div className="glass-primary card card-stock">
+                    <h3>$ {parseInt(currentPrice).toFixed(2)}</h3>
+                    <span className={`text-${color}`}>
+                        <img
+                            width="30px"
+                            src={isPositive ? arrowUp : arrowDown}
+                            alt="StockStalker Arrow"
+                        />{' '}
+                        $ {changeSinceOpen} ({changePercentage}%)
+                    </span>
+                    <div className={`chart glass-${color}`}>
+                        <LineGraph
+                            prediction={isPositive}
+                            graphData={graphData}
+                            baseline={baseline}
+                            timeFrame={timeFrame}
+                        />
+                    </div>
+                    <TimeFrameButtons
+                        prediction={isPositive}
+                        selectedTimeFrame={timeFrame}
+                        toggleTimeFrame={(t: string) => setTimeFrame(t)}
+                    />
                 </div>
-                <TimeFrameButtons
-                    prediction={prediction}
-                    selectedTimeFrame={timeFrame}
-                    toggleTimeFrame={(t: string) => setTimeFrame(t)}
-                />
-            </div>
-        </>
-    )
+            </>
+        )
+    }
 }
 
 export default StockInfoChart
