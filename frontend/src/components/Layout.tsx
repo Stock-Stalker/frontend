c7663b2828350a425917c19977b1188969603d5f
Starlight Romero <starlightromero@protonmail.com>
2021-03-19 11:58:54 -0700

    [update] linted backend

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -11,20 +11,19 @@ const Layout: React.FC<LayoutProps> = (props) => {
     return (
         <IonPage>
             <IonHeader>
-                    <div className="header-content">
-                        <img
-                            src={logo}
-                            className="logo"
-                            width="150px"
-                            alt="Stock Stalker Logo"
-                        />
-                        {
-                            props.searchbar &&
-                            <div className="searchbar-container">
-                                <SearchBar />
-                            </div>
-                        }
-                    </div>
+                <div className="header-content">
+                    <img
+                        src={logo}
+                        className="logo"
+                        width="150px"
+                        alt="Stock Stalker Logo"
+                    />
+                    {props.searchbar && (
+                        <div className="searchbar-container">
+                            <SearchBar />
+                        </div>
+                    )}
+                </div>
             </IonHeader>
             <IonContent fullscreen>
                 {props.children && props.children}
