06db729b6bc64686e5057a8597becdbc0d666b72
Starlight Romero <starlightromero@protonmail.com>
2021-03-18 20:10:31 -0700

    [update] api fix baseURL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_URL,
+    baseURL: process.env.REACT_APP_URI,
 })
 
 api.defaults.headers.common.Authorization = `Bearer ${localStorage.getItem(
