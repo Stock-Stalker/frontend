2229a71e218300e6f208e9430fce1dde8ca27013
tsamantanis <philip.tsamantanis@icloud.com>
2021-03-19 17:06:49 -0700

    Redux actions: watchlist setup

---
+++ frontend/src/actions/watchlist.tsx
@@ -0,0 +1,41 @@
+import api from '../api';
+// import axios from 'axios';
+interface Stock {
+    _id: string
+    symbol: string
+    companyName: string
+}
+
+export const setWatchlist = (watchlist: Array<Stock>) => {
+    return {
+        type: 'SET_WATCHLIST',
+        payload: watchlist
+    };
+};
+
+export const loadWatchlist = () => {
+    // axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
+    console.log('world')
+    // return async (dispatch: any) => {
+    //     try {
+    //         const res = await api.get('/user/watchlist')
+    //         dispatch(setWatchlist(res.data));
+    //     } catch (error) {
+    //         if (typeof error.response !== 'undefined' && error.response.status === 401) {
+    //             localStorage.removeItem('token');
+    //             window.location.href = '/signin';
+    //         }
+    //     }
+    // }
+    return (dispatch: any) => {
+        console.log('!')
+        api.get('/user/watchlist').then((res: any) => {
+            return dispatch(setWatchlist(res.data.watchlist));
+        }).catch((error: any) => {
+            if (typeof error.response !== 'undefined' && error.response.status === 401) {
+                localStorage.removeItem('token');
+                window.location.href = '/signin';
+            }
+        })
+    }
+}
