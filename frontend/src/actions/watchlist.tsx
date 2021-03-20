c5449aa84710c349884ac2754f8506055cc77233
tsamantanis <philip.tsamantanis@icloud.com>
2021-03-19 17:17:22 -0700

    Fix load watchlist route

--- frontend/src/actions/watchlist.tsx
+++ frontend/src/actions/watchlist.tsx
@@ -16,26 +16,26 @@ export const setWatchlist = (watchlist: Array<Stock>) => {
 export const loadWatchlist = () => {
     // axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
     console.log('world')
-    // return async (dispatch: any) => {
-    //     try {
-    //         const res = await api.get('/user/watchlist')
-    //         dispatch(setWatchlist(res.data));
-    //     } catch (error) {
-    //         if (typeof error.response !== 'undefined' && error.response.status === 401) {
-    //             localStorage.removeItem('token');
-    //             window.location.href = '/signin';
-    //         }
-    //     }
-    // }
-    return (dispatch: any) => {
-        console.log('!')
-        api.get('/user/watchlist').then((res: any) => {
-            return dispatch(setWatchlist(res.data.watchlist));
-        }).catch((error: any) => {
+    return async (dispatch: any) => {
+        try {
+            const res = await api.get('/user/watchlist')
+            dispatch(setWatchlist(res.data));
+        } catch (error) {
             if (typeof error.response !== 'undefined' && error.response.status === 401) {
                 localStorage.removeItem('token');
                 window.location.href = '/signin';
             }
-        })
+        }
     }
+    // return (dispatch: any) => {
+    //     console.log('!')
+    //     api.get('/user/watchlist').then((res: any) => {
+    //         return dispatch(setWatchlist(res.data.watchlist));
+    //     }).catch((error: any) => {
+    //         if (typeof error.response !== 'undefined' && error.response.status === 401) {
+    //             localStorage.removeItem('token');
+    //             window.location.href = '/signin';
+    //         }
+    //     })
+    // }
 }
