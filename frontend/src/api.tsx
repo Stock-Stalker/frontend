5ee03a0174df7d8ac13817e11abda0bad416e123
tsamantanis <philip.tsamantanis@icloud.com>
2021-03-22 12:08:18 -0700

    Temporary auth workaround

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,20 +1,21 @@
 import axios from 'axios'
-import store from './store'
+// import store from './store'
 
 const api: any = axios.create({
     baseURL: process.env.REACT_APP_URI,
 })
 
-let savedToken: string | null = null;
+// let savedToken: string | null = null;
 
-const setToken = () => {
-    const token: string = store.getState().token;
-    if (token && token !== savedToken) {
-        api.defaults.headers.common.Authorization = 'Bearer ' + token
-        savedToken = token
-    }
-};
-
-store.subscribe(setToken)
+// const setToken = () => {
+//     const token: string = store.getState().token;
+//     if (token && token !== savedToken) {
+//         api.defaults.headers.common.Authorization = 'Bearer ' + localStorage.getItem('token')
+//         savedToken = token
+//     }
+// };
+//
+// store.subscribe(setToken)
+api.defaults.headers.common.Authorization = 'Bearer ' + localStorage.getItem('token')
 
 export default api
