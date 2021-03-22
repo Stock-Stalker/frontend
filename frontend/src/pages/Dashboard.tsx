6e8dcb9ec3bfade3ba8d716dc94e2baabdcfc696
tsamantanis <philip.tsamantanis@icloud.com>
2021-03-22 10:59:06 -0700

    Add dashboard container

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import Watchlist from '../components/Watchlist'
-
+import './Dashboard.css'
 const Welcome: React.FC = () => {
     return (
         <Layout searchbar={true}>
-            <div className="popular-stocks">
+            <div className="dashboard-container">
+                <div className="popular-stocks">
+                </div>
+                <Watchlist />
             </div>
-            <Watchlist />
         </Layout>
     )
 }
