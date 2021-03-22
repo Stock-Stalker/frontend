22051d3b45b0c4cb1181657487f6e7f97e67bacb
tsamantanis <philip.tsamantanis@icloud.com>
2021-03-22 16:24:30 -0700

    Fully functional watchlist toggle

--- frontend/src/components/WatchlistToggle.tsx
+++ frontend/src/components/WatchlistToggle.tsx
@@ -1,27 +1,39 @@
 import React from 'react'
+import { useSelector, connect } from 'react-redux';
 import { IonIcon } from '@ionic/react'
 import { star, starOutline } from 'ionicons/icons'
 import api from '../api'
+import { setWatchlist } from '../actions/watchlist'
 import './WatchlistToggle.css'
 
+interface WatchlistToggleState {
+    token: string
+    watchlist: Stock[]
+}
+
+interface Stock {
+    _id: string
+    symbol: string
+    companyName: string
+}
+
 interface WatchlistToggleProps {
-    inWatchlist: boolean
+    setWatchlist: (watchlist: Stock[]) => void
     symbol: string
     prediction: boolean
 }
 
-const WatchlistToggle: React.FC<WatchlistToggleProps> = ({
-    inWatchlist,
-    symbol,
-    prediction,
-}) => {
+const WatchlistToggle: React.FC<WatchlistToggleProps> = (props) => {
+    const { symbol, prediction } = props
+    const watchlist = useSelector((state: WatchlistToggleState) => state.watchlist ? state.watchlist.map(item => item.symbol) : [])
     const color = prediction ? 'tertiary' : 'secondary'
 
     async function toggleWatchlist() {
         try {
-            await api.patch('/user/watchlist', {
+            const res = await api.patch('/user/watchlist', {
                 symbol: symbol,
             })
+            props.setWatchlist(res.data)
         } catch (error) {
             console.error(error)
         }
@@ -29,7 +41,7 @@ const WatchlistToggle: React.FC<WatchlistToggleProps> = ({
 
     return (
         <IonIcon
-            icon={ inWatchlist ? star : starOutline }
+            icon={ watchlist.includes(symbol) ? star : starOutline }
             size="large"
             color={ color }
             className="watchlist-toggle"
@@ -38,4 +50,4 @@ const WatchlistToggle: React.FC<WatchlistToggleProps> = ({
     )
 }
 
-export default WatchlistToggle
+export default connect(null,  { setWatchlist }) (WatchlistToggle);
