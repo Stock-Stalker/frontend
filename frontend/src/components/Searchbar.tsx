4023bc118804e0ba6a0c898777e7a9e7d95a15f3
tsamantanis <philip.tsamantanis@icloud.com>
2021-03-23 13:51:25 -0700

    Fix Stock is defined but never used error

--- frontend/src/components/Searchbar.tsx
+++ frontend/src/components/Searchbar.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import api from '../api'
-import { Stock } from '../types'
+// import { Stock } from '../types'
 import { IonSearchbar } from '@ionic/react'
 import './Searchbar.css'
 
