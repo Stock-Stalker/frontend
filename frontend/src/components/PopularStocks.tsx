5774c54c7e2866fe5771ed415ba621787a45072f
tsamantanis <philip.tsamantanis@icloud.com>
2021-03-23 11:26:45 -0700

    Add popular stocks

--- frontend/src/components/PopularStocks.tsx
+++ frontend/src/components/PopularStocks.tsx
@@ -11,7 +11,7 @@ const PopularStocks: React.FC = () => {
 
     const { data, isLoading, isError } = useQuery('popularStock', fetchStockData, {
             refetchOnWindowFocus: false,
-            enabled: false // needed to handle refetchs manually
+            // enabled: false // needed to handle refetchs manually
         }
     )
 
@@ -20,17 +20,18 @@ const PopularStocks: React.FC = () => {
     } else if (isError) {
         return <h3>Something Went Wrong</h3>
     } else {
+        console.log(data)
         return (
             <div className="popular-stocks">
                 <h2>Popular Stocks</h2>
-                { data.map((stock: any) => {
+                { data && data.map((stock: any) => {
                     return (
                         <StockEllipse
-                            key={ Object.keys(stock)[0] }
+                            key={ stock.symbol }
                             companyName=""
-                            symbol={ Object.keys(stock)[0] }
-                            currentPrice={ parseFloat(stock[Object.keys(stock)[0]]).toFixed(2) }
-                            prediction={1}
+                            symbol={ stock.symbol }
+                            currentPrice={ stock.currentPrice }
+                            prediction={ stock.prediction }
                         />
                     )
                 } ) }
